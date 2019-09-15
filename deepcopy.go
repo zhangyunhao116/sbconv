7d73ccc85b8865f15ff91836858d979a3a16327f
ZYunH <zyunhjob@163.com>
2019-09-15 22:19:23 +0800

    Simplify DeepCopyString

--- deepcopy.go
+++ deepcopy.go
@@ -13,11 +13,10 @@ func DeepCopyBytes(b []byte) []byte {
 
 // Deep copy input string.
 //
-// This function is used in case the input string
+// This function is used in case of the input string
 // is converted by BytesToString, this means it is
 // not a real string, but share an underlying array with
 // a byte slice, use this function to create a real string.
 func DeepCopyString(s string) string {
-	b := *(*[]byte)(unsafe.Pointer(&s))
-	return string(b)
+	return string(*(*[]byte)(unsafe.Pointer(&s)))
 }
